cab487114bb33ad13f4c9491135261ce8d181e03
Francesco Agosta <fagosta@bordeaux-inp.fr>
2026-06-13 14:13:38 +0000

    Review: deleted unused .sh files

--- clean_outputs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ===========================================================================
-# Clean output directory
-# ===========================================================================
-
-echo "[clean] Removing outputs folder..."
-rm -rf output
-echo "[clean] Output directory cleaned successfully."
